ffc6409fb5d260ec251a1a7b51643baef7ed6d09
HenriqueFreire <henriquefreire@outlook.com.br>
2022-12-31 03:24:35 -0300

    Iniciando a parte 2 do Stage-4

---
+++ Nível 4/Stage-4 part2/VisaoGeralDessaEtapa.js
@@ -0,0 +1,15 @@
+/*
+
+Lógica
+- Maneira de pensar, racionar
+- Organização racionaldas ideias, dos pensamentos
+
+Algoritmos
+- Sequência de passos para determinar uma tarefa
+
+Javascript
+- Linguagem da web
+- Linguagem interpretada, dinâmica, multi paradigma, moderna
+- Front-End, Back-end, mobile, desktop
+- Comunidade e oportunidades
+*/
