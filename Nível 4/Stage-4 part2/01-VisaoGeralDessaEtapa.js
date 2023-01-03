741e89441e3ea3641db8ae18e13a4f95dd9cda24
HenriqueFreire <henriquefreire@outlook.com.br>
2023-01-03 09:08:03 -0300

    Segunda aula deste módulo concluído

--- Nível 4/Stage-4 part2/01-VisaoGeralDessaEtapa.js
+++ Nível 4/Stage-4 part2/01-VisaoGeralDessaEtapa.js
@@ -12,4 +12,15 @@ Javascript
 - Linguagem interpretada, dinâmica, multi paradigma, moderna
 - Front-End, Back-end, mobile, desktop
 - Comunidade e oportunidades
+
+A metodologia Learning By Doing
+- Aprender fazendo
+
+O que vamos aprender nesta etapa
+- Variáveis
+- Tipos e estruturas de dados
+- Funções
+- Estrutura de repetição
+- Operadores matemáticos e lógicos
+
 */
